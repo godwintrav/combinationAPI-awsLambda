3e4f77babdccefddbbec939fce66a040b8bee712
Godwin Odenigbo <godwintrav@gmail>
2022-12-02 20:20:50 +0000

    fixed error log

--- src/functions/combinationAPI/index.ts
+++ src/functions/combinationAPI/index.ts
@@ -14,7 +14,7 @@ export const handler = async (event: APIGatewayProxyEvent) => {
             return formatJSONResponse({
                 statusCode: 400,
                 data: {
-                    message: "Missing currency query parameter",
+                    message: "Missing currency query paramete",
                 },
             });
         }
